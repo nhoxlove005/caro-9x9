df0c8cb545139b8ff9e2da86a7ef0fa33b9b725a
nhoxlove005 <75316371+nhoxlove005@users.noreply.github.com>
2024-01-12 11:35:32 +0700

    Add files via upload

---
+++ Gomoku.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+class Gomoku:
+    def __init__(self) :
+        self.row_count = 19
+        self.column_count = 15
+        self.action_size = self.row_count * self.column_count
+
+    def __repr__(self):
+        return "Gomoku"
+
+    def get_initial_state(self):
+        # return initial board (numpy board)
+        return np.zeros((self.row_count, self.column_count))
+
+    def get_next_state(self, state, action, player):
+        # return board after applying action
+        row = action // self.column_count
+        col = action % self.column_count
+        state[row][col] = player
+        return state
+    
+    def get_valid_moves(self, state):
+        # return valid moves
+        return (state.reshape(-1) == 0).astype(np.uint16)
+    
+    def check_winner(self, state, action):
+        if action == None:
+            return False
+        # return True if player wins
+        row = action // self.column_count
+        col = action % self.column_count
+        player = state[row][col]
+
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+        for dr, dc in directions:
+            count = 1
+            for dir in [-1, 1]:
+                r, c = row + dir * dr, col + dir * dc
+                while (0 <= r < self.row_count and 0 <= c < self.column_count and state[r][c] == player):
+                    count += 1
+                    r, c = r + dir * dr, c + dir * dc
+            if count >= 5:
+                return True
+
+        return False
+    
+    def get_value_and_terminated(self, state, action):
+        # return value and terminal
+        if self.check_winner(state, action):
+            return 1, True
+        if np.sum(self.get_valid_moves(state)) == 0:
+            return 0, True
+        return 0, False
+    
+    def get_opponent(self, player):
+        # return opponent
+        return -player
+    
+    def get_opponent_value(self, value):
+        # return opponent value
+        return -value
+    
+    def change_perspective(self, state, player):
+        # return state after changing perspective
+        return state * player
+    
+    def get_encoded_state(self, state):
+        # return encoded state
+        encoded_state = np.stack(
+            (state == -1, state == 0, state == 1)
+        ).astype(np.float32)
+
+        if len(state.shape) == 3:
+            encoded_state = np.swapaxes(encoded_state, 0, 1)
+        
+        return encoded_state
---
+++ Play.py
@@ -0,0 +1,58 @@
+from Gomoku import Gomoku
+from TicTacToe import TicTacToe
+from MCTS import MCTS
+from ResNet import ResNet
+
+import numpy as np
+import torch
+
+player = 1
+
+
+game = Gomoku()
+
+args = {
+    'C': 2,
+    'num_searches': 600,
+    'dirichlet_epsilon': 0.,
+    'dirichlet_alpha': 0.3
+}
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = ResNet(game, 18, 256, device)
+
+mcts = MCTS(game, args, model)
+
+state = game.get_initial_state()
+
+while True:
+    
+    if player == 1:
+        print(state)
+        valid_moves = game.get_valid_moves(state)
+        print("valid_moves", [i for i in range(game.action_size) if valid_moves[i] == 1])
+        action = int(input(f"{player}:"))
+
+        if valid_moves[action] == 0:
+            print("action not valid")
+            continue
+            
+    else:
+        neutral_state = game.change_perspective(state, player)
+        mcts_probs = mcts.search(neutral_state)
+        action = np.argmax(mcts_probs)
+        
+    state = game.get_next_state(state, action, player)
+    
+    value, is_terminal = game.get_value_and_terminated(state, action)
+    
+    if is_terminal:
+        print(state)
+        if value == 1:
+            print(player, "won")
+        else:
+            print("draw")
+        break
+        
+    player = game.get_opponent(player)
